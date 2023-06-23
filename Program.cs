9130f94a30aec8c9e403e14cb093729553788747
yaron <yaron@YARONIX>
2023-06-23 16:59:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the board size: ");
+            int boardSize = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Computer vs player = 1, Player vs player = 2, Computer vs computer = 3.");
+            int choose = int.Parse(Console.ReadLine());
+
+            XOBoard board = new XOBoard(boardSize);
+            Console.WriteLine();
+            Random random = new Random();
+            board.PrintBoard();
+
+            int firstPlayer = random.Next(1, 3);
+            int secondPlayer;
+            if (firstPlayer == 1)
+            {
+                Console.WriteLine("Player X starts first.\n");
+                secondPlayer = 2;
+            }
+            else
+            {
+                Console.WriteLine("Player O starts first.\n");
+                secondPlayer = 1;
+            }
+
+
+            int i, j;
+
+            // Game loop
+            while (board.BoardStatus() == -1)
+            {
+                // First player
+                if (choose != 3)
+                {
+                    Console.WriteLine("Player {0}, enter place (column, row): ", board.GetPlayerChar(firstPlayer));
+                    Console.Write("column: ");
+                    j = int.Parse(Console.ReadLine());
+                    Console.Write("row: ");
+                    i = int.Parse(Console.ReadLine());
+                    if (board.IsEmpty(i, j))
+                    {
+                        board.AddChar(board.GetPlayerChar(firstPlayer), i, j);
+                    }
+                    else
+                    {
+                        while (board.IsEmpty(i, j) == false)
+                        {
+                            Console.WriteLine("The place is occupied. Please enter other column, row values: ");
+                            Console.Write("column: ");
+                            j = int.Parse(Console.ReadLine());
+                            Console.Write("row: ");
+                            i = int.Parse(Console.ReadLine());
+                        }
+                        board.AddChar(board.GetPlayerChar(firstPlayer), i, j);
+                    }
+                    if (board.BoardStatus() == firstPlayer)
+                    {
+                        Console.WriteLine();
+                        board.PrintBoard();
+                        Console.WriteLine("Player {0} has won!!!", board.GetPlayerChar(firstPlayer));
+                        break;
+                    }
+                    Console.WriteLine();
+                    board.PrintBoard();
+
+                    if (board.BoardStatus() == 0)
+                    {
+                        Console.WriteLine("Draw");
+                        break;
+                    }
+                }else
+                {
+                    int rndI = random.Next(0, boardSize), rndJ = random.Next(0, boardSize);
+                    while (board.IsEmpty(rndI, rndJ) == false)
+                    {
+                        rndI = random.Next(0, boardSize); rndJ = random.Next(0, boardSize);
+                    }
+                    board.AddChar(board.GetPlayerChar(firstPlayer), rndI, rndJ);
+                    Console.WriteLine("Computer plays j={0}, i={1}.", rndJ, rndI);
+
+                    if (board.BoardStatus() == firstPlayer)
+                    {
+                        board.PrintBoard();
+                        Console.WriteLine();
+                        Console.WriteLine("Player {0} has won!!!", board.GetPlayerChar(firstPlayer));
+                        break;
+                    }
+                    Console.WriteLine();
+                    board.PrintBoard();
+
+                    if (board.BoardStatus() == 0)
+                    {
+                        Console.WriteLine("Draw");
+                        break;
+                    }
+                }
+
+                // Second player
+                if (choose == 2)
+                {
+                    Console.WriteLine("Player {0}, enter place (column, row): ", board.GetPlayerChar(secondPlayer));
+                    Console.Write("column: ");
+                    j = int.Parse(Console.ReadLine());
+                    Console.Write("row: ");
+                    i = int.Parse(Console.ReadLine());
+                    if (board.IsEmpty(i, j))
+                    {
+                        board.AddChar(board.GetPlayerChar(secondPlayer), i, j);
+                    }
+                    else
+                    {
+                        while (board.IsEmpty(i, j) == false)
+                        {
+                            Console.WriteLine("The place is occupied. Please enter other column, row values: ");
+                            Console.Write("column: ");
+                            j = int.Parse(Console.ReadLine());
+                            Console.Write("row: ");
+                            i = int.Parse(Console.ReadLine());
+                        }
+                        board.AddChar(board.GetPlayerChar(secondPlayer), i, j);
+                    }
+                    if (board.BoardStatus() == secondPlayer)
+                    {
+                        board.PrintBoard();
+                        Console.WriteLine();
+                        Console.WriteLine("Player {0} has won!!!", board.GetPlayerChar(secondPlayer));
+                        break;
+                    }
+                    Console.WriteLine();
+                    board.PrintBoard();
+
+                    if (board.BoardStatus() == 0)
+                    {
+                        Console.WriteLine("Draw");
+                        break;
+                    }
+                }
+                else
+                {
+                    int rndI = random.Next(0, boardSize), rndJ = random.Next(0, boardSize);
+                    while (board.IsEmpty(rndI, rndJ) == false)
+                    {
+                        rndI = random.Next(0, boardSize); rndJ = random.Next(0, boardSize);
+                    }
+                    board.AddChar(board.GetPlayerChar(secondPlayer), rndI, rndJ);
+                    Console.WriteLine("Computer plays j={0}, i={1}.", rndJ, rndI);
+
+                    if (board.BoardStatus() == secondPlayer)
+                    {
+                        board.PrintBoard();
+                        Console.WriteLine();
+                        Console.WriteLine("Player {0} has won!!!", board.GetPlayerChar(secondPlayer));
+                        break;
+                    }
+                    Console.WriteLine();
+                    board.PrintBoard();
+
+                    if (board.BoardStatus() == 0)
+                    {
+                        Console.WriteLine("Draw");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ XOBoard.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TicTacToe
+{
+    internal class XOBoard
+    {
+        // The 2D array of characters
+        char[,] board;
+
+        // Methods
+        public XOBoard(int size)
+        {
+            board = new char[size, size];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = '-';
+                }
+            }
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j]);
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public int GetPlayerNum(char c)
+        {
+            if (c == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+
+        public char GetPlayerChar(int num)
+        {
+            if (num == 1)
+            {
+                return 'X';
+            }
+            else
+            {
+                return 'O';
+            }
+        }
+
+        public bool IsEmpty(int i, int j)
+        {
+            if (board[i, j] == '-')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddChar(char c, int i, int j)
+        {
+            board[i, j] = c;
+        }
+
+        public int BoardStatus()
+        {
+            int cnt = 0;
+
+            // Player #1 - X
+
+            // rows
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 'X')
+                    {
+                        cnt++;
+                    }
+                    if (cnt == board.GetLength(0))
+                    {
+                        return 1;
+                    }
+                }
+                cnt = 0;
+            }
+            // columns
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    if (board[i, j] == 'X')
+                    {
+                        cnt++;
+                    }
+                    if (cnt == board.GetLength(0))
+                    {
+                        return 1;
+                    }
+                }
+                cnt = 0;
+            }
+            // slants
+            cnt = 0;
+            for (int i = 0, j = 0; i < board.GetLength(0); i++, j++)
+            {
+                if (board[i, j] == 'X')
+                {
+                    cnt++;
+                }
+                if (cnt == board.GetLength(0))
+                {
+                    return 1;
+                }
+            }
+            cnt = 0;
+            for (int i = 0, j = board.GetLength(1) - 1; i < board.GetLength(0); i++, j--)
+            {
+                if (board[i, j] == 'X')
+                {
+                    cnt++;
+                }
+                if (cnt == board.GetLength(0))
+                {
+                    return 1;
+                }
+            }
+            cnt = 0;
+
+            // Player #2 - O
+
+            // rows
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 'O')
+                    {
+                        cnt++;
+                    }
+                    if (cnt == board.GetLength(0))
+                    {
+                        return 2;
+                    }
+                }
+                cnt = 0;
+            }
+            // columns
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+                    if (board[i, j] == 'O')
+                    {
+                        cnt++;
+                    }
+                    if (cnt == board.GetLength(0))
+                    {
+                        return 2;
+                    }
+                }
+                cnt = 0;
+            }
+            // slants
+            for (int i = 0, j = 0; i < board.GetLength(0); i++, j++)
+            {
+                if (board[i, j] == 'O')
+                {
+                    cnt++;
+                }
+                if (cnt == board.GetLength(0))
+                {
+                    return 2;
+                }
+            }
+            cnt = 0;
+            for (int i = 0, j = board.GetLength(1) - 1; i < board.GetLength(0); i++, j--)
+            {
+                if (board[i, j] == 'O')
+                {
+                    cnt++;
+                }
+                if (cnt == board.GetLength(0))
+                {
+                    return 2;
+                }
+            }
+            cnt = 0;
+
+            // Full board
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == 'X' || board[i, j] == 'O')
+                    {
+                        cnt++;
+                    }
+                    if (cnt == Math.Pow(board.GetLength(0), 2))
+                    {
+                        return 0;
+                    }
+                }
+            }
+
+            // No winner and no full
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int[] CompCell()
+        {
+            int cnt = 0;
+            int[] cell = new int[2];
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == '-')
+                    {
+                        cell[0] = i;
+                        cell[1] = j;
+                        return cell;
+                    }
+                }
+            }
+            return cell;
+        }
+    }
+}
